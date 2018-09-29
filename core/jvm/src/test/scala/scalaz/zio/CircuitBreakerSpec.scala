1e8c6456ecad4e91215fa01bc3da23eb01019988
wi101 <wiemzin@gmail.com>
2018-09-29 15:58:49 +0200

    added test Co-authored-by: ahoy-jon <jonathan.winandy@gmail.com>

--- core/jvm/src/test/scala/scalaz/zio/CircuitBreakerSpec.scala
+++ core/jvm/src/test/scala/scalaz/zio/CircuitBreakerSpec.scala
@@ -1,5 +1,46 @@
 package scalaz.zio
 
-class CircuitBreakerSpec {
+import scalaz.zio.CircuitBreaker.Open
 
+class CircuitBreakerSpec extends AbstractRTSSpec {
+  def is = "CircuitBreakerSpec".title ^ s2"""
+     protect must work correctly $e
+    """
+
+  sealed trait State
+  object State {
+    case object Failed      extends State
+    case object Success     extends State
+    case object CircuitOpen extends State
+  }
+
+  import State._
+
+  def e =
+    unsafeRun(for {
+      circuit <- CircuitBreaker(2, IO.fail(CircuitOpen))
+      v       <- circuit.protect(IO.fail(Failed)).attempt
+      nbR1    <- circuit.nbRemainingFailure
+      w       <- circuit.protect(IO.point(Success)).attempt
+      nbR2    <- circuit.nbRemainingFailure
+      _       <- circuit.protect(IO.fail(Failed)).attempt
+      nbR3    <- circuit.nbRemainingFailure
+      _       <- circuit.protect(IO.fail(Failed)).attempt
+      nbR4    <- circuit.nbRemainingFailure
+      status  <- circuit.status
+      x       <- circuit.protect(IO.point(Success)).attempt
+      nbR5    <- circuit.nbRemainingFailure
+      y       <- circuit.protect(IO.fail(Failed)).attempt
+    } yield {
+      (v must beLeft[State](Failed))
+        .and(nbR1 must_=== 1)
+        .and(w must beRight[State](Success))
+        .and(nbR2 must_=== 2)
+        .and(nbR3 must_=== 1)
+        .and(nbR4 must_=== 0)
+        .and(status must_=== Open)
+        .and(nbR5 must_=== 0)
+        .and(x must beLeft[State](CircuitOpen))
+        .and(y must beLeft[State](CircuitOpen))
+    })
 }
--- core/shared/src/main/scala/scalaz/zio/CircuitBreaker.scala
+++ core/shared/src/main/scala/scalaz/zio/CircuitBreaker.scala
@@ -1,23 +1,17 @@
 package scalaz.zio
 
-import CircuitBreaker.{Closed, Open, Status}
+import CircuitBreaker.{ Closed, Open, Status }
 
-final class CircuitBreaker[+BreakingFailure](ref:        Ref[CircuitBreakerStatus],
+final class CircuitBreaker[+BreakingFailure](ref: Ref[CircuitBreakerStatus],
                                              initStatus: CircuitBreakerStatus,
-                                             whenOpen:   IO[BreakingFailure, Nothing]) {
-  def protect[E >: BreakingFailure, A](io: IO[E, A]): IO[E, A] = {
+                                             whenOpen: IO[BreakingFailure, Nothing]) {
+  def protect[E >: BreakingFailure, A](io: IO[E, A]): IO[E, A] =
     for {
       status <- this.status
-      x <- {
-        if (status == Closed) {
-          io.redeem(e => ref.update(_.incFail) *> IO.fail(e), b => ref.set(initStatus) *> IO.point(b))
-        } else whenOpen
-      }
-    } yield {
-      x
-
-    }
-  }
+      x <- if (status == Closed)
+            io.redeem(e => ref.update(_.incFail) *> IO.fail(e), b => ref.set(initStatus) *> IO.point(b))
+          else whenOpen
+    } yield x
 
   def status: IO[Nothing, Status] = ref.get.map(_.status)
 
@@ -26,26 +20,23 @@ final class CircuitBreaker[+BreakingFailure](ref:        Ref[CircuitBreakerStatu
 
 case class CircuitBreakerStatus(nbRemainingFailure: Long, status: Status = Closed) {
 
-  def incFail: CircuitBreakerStatus = {
+  def incFail: CircuitBreakerStatus =
     if (nbRemainingFailure > 1) {
       copy(nbRemainingFailure = nbRemainingFailure - 1)
     } else {
       copy(nbRemainingFailure = nbRemainingFailure - 1, status = Open)
     }
-  }
 
 }
 
 object CircuitBreaker {
 
   sealed trait Status
-
-  object Closed extends Status
-
-  object Open extends Status
+  case object Closed extends Status
+  case object Open   extends Status
 
   def apply[BreakingFailure](nbConsecutiveFailure: Long,
-                             whenOpen:             IO[BreakingFailure, Nothing]): IO[Nothing, CircuitBreaker[BreakingFailure]] = {
+                             whenOpen: IO[BreakingFailure, Nothing]): IO[Nothing, CircuitBreaker[BreakingFailure]] = {
     val initStatus: CircuitBreakerStatus = CircuitBreakerStatus(nbConsecutiveFailure)
     Ref(initStatus).map(ref => {
       new CircuitBreaker[BreakingFailure](ref, initStatus, whenOpen)
